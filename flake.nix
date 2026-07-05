f03c4e15a718f1a588e47fe409fb501faf45533a
dogth <dogth@kitteth.com>
2026-07-06 01:41:58 +0300

    ci: setup flake

--- flake.nix
+++ flake.nix
@@ -1,73 +1,82 @@
 {
+  description = "Vulkan (MoltenVK) development shell for macOS";
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    pre-commits.url = "github:cachix/git-hooks.nix";
-    utils.url = "github:numtide/flake-utils";
+    flake-utils.url = "github:numtide/flake-utils";
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
   };
+
   outputs =
     {
       self,
       nixpkgs,
-      utils,
-      pre-commits,
-    }@inputs:
-    utils.lib.eachDefaultSystem (
+      flake-utils,
+      pre-commit-hooks,
+    }:
+    flake-utils.lib.eachDefaultSystem (
       system:
       let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
-      in
-      with pkgs;
-      {
-        checks = inputs.pre-commits.lib.${system}.run {
-          src = ./.;
-          default_stages = [
-            "manual"
-            "pre-push"
-          ];
+        pkgs = import nixpkgs { inherit system; };
 
-          hooks = {
-            clang-format = {
-              enable = true;
-              types_or = lib.mkForce [
-                "c"
-                "c++"
-              ];
-            };
-            compat = {
-              enable = true;
-              name = "CXX98 Compatibility Tests";
-              entry = ''cmake -B build -DBUILD_TESTING=ON -DFAIL_ON_REGRESSION_FAILURE=ON --fresh'';
-              language = "system";
-              stages = [ "pre-push" ];
-              pass_filenames = false;
-            };
-            unit-test = {
-              enable = true;
-              name = "Unit Tests";
-              files = "\\.(cpp|hpp)$";
-              entry = ''ctest --test-dir build --output-on-failure '';
-              language = "system";
-              stages = [ "pre-push" ];
-              pass_filenames = false;
-            };
-
-            commitizen.enable = true;
-          };
-        };
-
-        devShells.default = mkShell {
-          name = "MoltenVK playground development Shell";
-          inherit (self.checks.${system}) shellHook;
-
-          buildInputs = self.checks.${system}.enabledPackages;
-
-          packages = with pkgs; [
+        vulkanPackages =
+          with pkgs;
+          [
             vulkan-headers
             vulkan-loader
             vulkan-validation-layers
+            vulkan-tools
+            moltenvk
+            shaderc
+            glslang
+            spirv-tools
+            spirv-cross
+          ]
+          ++ pkgs.lib.optionals (!pkgs.stdenv.isDarwin) [
+            vulkan-extension-layer
           ];
+        buildTools = with pkgs; [
+          cmake
+          ninja
+          pkg-config
+          glfw3
+          glm
+        ];
+
+        testTools = with pkgs; [
+          catch2_3
+        ];
+
+        devTools = with pkgs; [
+          commitizen
+          git
+        ];
+      in
+      {
+        checks = {
+          pre-commit-check = pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              commitizen.enable = true;
+              clang-format.enable = true;
+              cmake-format.enable = true;
+            };
+          };
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = vulkanPackages ++ buildTools ++ testTools ++ devTools;
+
+          shellHook = ''
+            ${self.checks.${system}.pre-commit-check.shellHook}
+
+            export VULKAN_SDK="${pkgs.vulkan-headers}"
+            export VK_ICD_FILENAMES="${pkgs.moltenvk}/share/vulkan/icd.d/MoltenVK_icd.json"
+            export VK_LAYER_PATH="${pkgs.vulkan-validation-layers}/share/vulkan/explicit_layer.d"
+            export DYLD_LIBRARY_PATH="${pkgs.moltenvk}/lib''${DYLD_LIBRARY_PATH:+:$DYLD_LIBRARY_PATH}"
+
+            echo "Vulkan (MoltenVK) shell ready :3"
+          '';
         };
       }
     );
